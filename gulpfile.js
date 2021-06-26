8bf17b8ef6d4e168b3ccc11b310eda56c29fb595
Mikhail Klemin <mikhail.klemin@gmail.com>
2021-06-26 22:32:27 +0300

    change readme

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,20 @@ const browserSync = require('browser-sync').create();
 
 
 function style(){
-    return gulp.src("./components/sass/**/*.scss")
+    return gulp.src("./sass/**/*.scss")
     .pipe(sass())
-    .pipe(gulp.dest('./builds/dest/css'))
+    .pipe(gulp.dest('./dest/css'))
     .pipe(browserSync.stream());
 }
 
 function watch() {
     browserSync.init({
         server: {
-            baseDir:'./builds/dest/'
+            baseDir:'./dest/'
         }
     });
-    gulp.watch('./components/sass/**/*.scss', style);
-    gulp.watch('./builds/dest/*.html').on('change', browserSync.reload);
+    gulp.watch('./sass/**/*.scss', style);
+    gulp.watch('./dest/*.html').on('change', browserSync.reload);
 }
 
 exports.style = style;
